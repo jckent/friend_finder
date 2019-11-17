ea65e6c2315c52821adca278439f80e4d5591755
jckent <chasekent30@gmail.com>
2019-11-17 15:03:25 -0500

    finished friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+      name: "Jessica",
+      photo: "https://www.randomlists.com/img/people/bethenny_frankel.webp",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+      ]
+    },
+    {
+      name: "Steve",
+      photo: "https://data.whicdn.com/images/283075588/original.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2"
+      ]
+    },
+    {
+      name: "Solomon",
+      photo: "https://i.pinimg.com/236x/e1/29/43/e1294386faa370a8b61f637b9d0115c1--famous-men-famous-people.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Elizabeth",
+      photo: "https://scstylecaster.files.wordpress.com/2016/10/paris.png?w=712&h=400&crop=1",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Simon",
+      photo: "https://data.whicdn.com/images/227332090/original.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Jenna",
+      photo: "https://i.pinimg.com/236x/19/24/12/1924126f6be63fdda6304ebe5231d3c6--celebs-celebrities.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    }
+  
+  ];
+  
+
+  module.exports = friends;
